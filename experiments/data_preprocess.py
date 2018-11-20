f7c9da2b93cdf25bd2737f15fe8526e4a57824b8
BhalchandraNaik <bhna5709@colorado.edu>
2018-11-19 17:32:28 -0700

    Completed data-preprocessing

--- experiments/data_preprocess.py
+++ experiments/data_preprocess.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from glob import glob
+import ast
 
-def list2numpy(points_list,size=1):
+def list2numpy(points_list,size=2):
 
     """
     Takes a list of points and converts it to a boolean
@@ -29,7 +31,50 @@ def list2numpy(points_list,size=1):
     X = np.array(fig.canvas.renderer._renderer)
     plt.close()
 
-    return X[:,:,1] == 255
+    return (~(X[:,:,1] == 255)).astype(int).flatten()
 
-df = pd.read_csv('C:\\Users\\bnaik\\Desktop\\CSCI_5622_Machine_Learning_Project\\data\\train_simplified\\bee.csv')
-list2numpy(df.drawing[7],size=4)
+def get_class_name(file_path):
+    i = len(file_path)-5
+    name = ''
+    while file_path[i]!='\\':
+        name = file_path[i]+name
+        i = i-1
+    return name
+
+def count_lines(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i-1
+
+def read_csvs(csv_files, nrows=1000):
+    df =  pd.concat([ pd.read_csv(file,nrows=nrows) for file in csv_files])
+    df.reset_index(inplace=True,drop=True)
+    df['drawing'] = df.drawing.apply(ast.literal_eval)
+    return df
+
+# Get all the files paths in the target folder
+data_dir = 'C:\\Users\\bnaik\\Desktop\\CSCI_5622_Machine_Learning_Project\\data\\temp_data\\'
+data_files = glob(data_dir+"*.csv")
+
+# get the class names
+class_names = [get_class_name(file_path) for file_path in data_files]
+# print(class_names)
+
+# Classes and data-points
+line_counts = [count_lines(file_path) for file_path in data_files]
+counts = pd.DataFrame({"line_counts":line_counts})
+counts["class"] = class_names
+# print(counts)
+
+# Read the data into a dataframe
+df = read_csvs(data_files, nrows = 10000)
+
+# Now convert the timestamped vector into an actually drawing
+
+# plt.imshow(list2numpy(df.drawing[7],size=4),cmap="gray")
+# plt.axis('off')
+# plt.show()
+df['drawing'] = df['drawing'].apply(list2numpy)
+df['drawing'] = df['drawing'].apply(np.array2string, threshold=40001)
+df.to_csv("C:\\Users\\bnaik\\Desktop\\CSCI_5622_Machine_Learning_Project\\data\\my_data_.csv")
